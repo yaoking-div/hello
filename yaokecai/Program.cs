70813740cd70a9b9c93669991b40b76408d7acfb
yaoking-div <1228479489@qq.com>
2020-06-23 21:57:29 +0800

    我的第一个提交代码

---
+++ yaokecai/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace yaokecai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            int a = 1;
+            int b = 2;
+            if (a > b)
+                c = b;
+            else;
+            c = a;
+            Console.WriteLine(c); ;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
